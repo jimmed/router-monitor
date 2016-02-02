e8430df3d837236f55428e1c8a5dd9cf1af57889
Jim O'Brien <jim@lens.io>
2016-02-02 14:55:05 +0000

    Add readable Stream for router status

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var scraper = require('./lib/sky/scraper')
 var findGateway = require('./lib/util/findGateway')
+var StatusStream = require('./lib/sky/stream')
 var inspect = require('util').inspect
 
 testFlight({
   host: findGateway(),
-  pass: process.env.ROUTER_PASS
+  pass: process.env.ROUTER_PASS,
+  timeout: 5000
 })
 
 function testFlight (opts) {
-  return scraper.getStatus(opts || {})
-    .then(res => console.log(inspect(res, { colors: true, depth: 5 })))
-    .catch(err => console.error(err.stack))
-    .delay(15000)
-    .finally(() => testFlight(opts))
+  var stream = new StatusStream(opts)
+
+  stream.on('readable', () => {
+    console.log(inspect(stream.read(), { colors: true, depth: 4 }))
+  })
 }
